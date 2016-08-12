e0efc56e509274949a6e8aa5725d54560fdb4a8d
adcpm <fabien@adcpm.com>
2016-08-12 20:04:05 +0700

    Commit

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var steemembed = require('./lib/steemembed');
 
-var embed = steemembed.get('https://www.periscope.tv/MoranAbdullah/1vOGwpLRNOdJB');
+var embed = steemembed.get('https://soundcloud.com/liluzivert/do-what-i-want-produced-by-maaly-raw-don-cannon');
 console.log(embed);
