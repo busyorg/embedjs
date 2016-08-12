fd894711583d603558362e0d4d643f2f37953df8
adcpm <fabien@adcpm.com>
2016-08-12 18:58:09 +0700

    Commit

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var steemembed = require('./lib/steemembed');
 
-var embed = steemembed.get('https://www.twitch.tv/johnnydarko420');
+var embed = steemembed.get('https://www.periscope.tv/MoranAbdullah/1vOGwpLRNOdJB');
 console.log(embed);
